4f0e4a137137ae5ffd58262ac5acbee23ce8ff1e
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2025-06-14 10:16:53 -0600

    Spring initializr - Proyecto Base

---
+++ src/test/java/com/montelongo/FindBook/FindBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montelongo.FindBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
